9ca75312ac89c4e097e176d652dd4934533dfae3
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-24 15:35:44 -0700

    socket setup

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require('./lib/app.js');
+const path = require('path');
+const express = require('express');
 const pool = require('./lib/utils/pool.js');
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: true
+});
 
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 7890;
+
+httpServer.listen(4000, () => {
+  console.log('http server on 4000');
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
@@ -12,3 +25,6 @@ process.on('exit', () => {
   console.log('Goodbye!');
   pool.end();
 });
+
+
+module.exports = io;
