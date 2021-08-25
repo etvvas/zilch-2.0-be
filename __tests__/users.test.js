7499a7ce34b8ad4c08f2bd8123fa20caac17797e
Chase <cabbott93@gmail.com>
2021-08-25 09:39:14 -0700

    initialize signup route, add test for signup

---
+++ __tests__/users.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('signs a user up', async () => {
+    const user = {
+      username: 'chase',
+      password: 'password',
+      avatar: 'Avatar.png'
+    };
+
+    const { body } = await request(app)
+      .post('/api/v1/signup')
+      .send(user);
+
+    expect(body).toEqual({
+      username: 'chase',
+      userId: '1',
+      avatar: 'Avatar.png' });
+  });
+});
