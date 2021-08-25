9bfcb3664d1613f6fabfc009a31c969f748fe053
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-25 10:54:53 -0700

    games getAllGames route, function, test all working

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -2,20 +2,29 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+const Game = require('../lib/models/Game.js')
 
 describe('Games tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+  const gameOne = {
+    firstUserId: '1',
+    secondUserId: '2',
+    timestampStart: '1:50'
+  }
+
+  const gameTwo = {
+    firstUserId: '2',
+    secondUserId: '3',
+    timestampStart: '2:50'
+  }
+
   it('POST a game', async () => {
     const res = await request(app)
       .post('/api/v1/games')
-      .send({
-        firstUserId: '1',
-        secondUserId: '2',
-        timestampStart: '1:50'
-      });
+      .send(gameOne);
 
     expect(res.body).toEqual({
       id: '1',
@@ -26,4 +35,14 @@ describe('Games tests', () => {
       timestampEnd: null
     });
   });
+
+  it('GET all games', async () => {
+      const game1 = await Game.insert(gameOne)
+      const game2 = await Game.insert(gameTwo)
+
+      const res = await request(app)
+        .get('/api/v1/games')
+
+    expect(res.body).toEqual([game1, game2])
+  })
 });
