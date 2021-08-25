674ef663e8a7b685290ff4341fdabfd740b5c1c7
Chase <cabbott93@gmail.com>
2021-08-25 14:23:28 -0700

    add verify test, change verify data return

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -102,6 +102,15 @@ describe.only('user routes', () => {
 
     expect(body.status).toBe(500);
     expect(body.message).toBe('jwt must be provided');
+
+    const newSignUp = await agent
+      .post('/api/v1/signup')
+      .send(user);
+
+    const verifyRes = await agent
+      .get('/api/v1/verify');
+
+    expect(verifyRes.body).toEqual({ ...newSignUp.body, iat: expect.any(Number) });
   });
 
 });
