65d21c034800eec255793289a4710858c0a43210
Katherine Tam <katherinemtam@gmail.com>
2021-08-26 08:51:56 -0700

    all games by user get route test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -51,4 +51,64 @@ describe('users routes', () => {
 
     expect(res.body).toEqual(userGame)
   })
+
+  test('outputs all games of a user via GET', async () => {
+
+    const user1 = await agent
+      .post('/api/v1/signup')
+      .send(userOne);
+
+    const user2 = await agent
+      .post('/api/v1/signup')
+      .send(userTwo); 
+
+    const user3 = await agent
+      .post('/api/v1/signup')
+      .send(userThree); 
+
+
+    const game1 = await Game.insert({
+      firstUserId: user1.body.userId.toString(),
+      secondUserId: user2.body.userId.toString(),
+      timestampStart: '1:50',
+      targetScore: 5000
+    })
+
+    const game2 = await Game.insert({
+      firstUserId: user2.body.userId.toString(),
+      secondUserId: user1.body.userId.toString(),
+      timestampStart: '2:50',
+      targetScore: 3000
+    })
+
+    const game3 = await Game.insert({
+      firstUserId: user2.body.userId.toString(),
+      secondUserId: user3.body.userId.toString(),
+      timestampStart: '5:00',
+      targetScore: 10000
+    })
+
+    const userGame1 = await UserGame.insert({
+      userId: user1.body.userId, 
+      gameId: game1.gameId
+    })
+
+    const userGame2 = await UserGame.insert({
+      userId: user2.body.userId, 
+      gameId: game1.gameId
+    })
+
+    const userGame3 = await UserGame.insert({
+      userId: user2.body.userId, 
+      gameId: game2.gameId
+    })
+
+    const res = await agent
+      .get(`/api/v1/users/${user2.body.userId}/games`)
+    
+    expect(res.body).toEqual([
+      {userId: user2.body.userId, ...game1}, 
+      {userId: user2.body.userId, ...game2}
+    ]);
+  });
 });
