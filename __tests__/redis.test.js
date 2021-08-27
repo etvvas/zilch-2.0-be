38bff9738c6314b2e7350a57f0395ec6916e1529
Chase <cabbott93@gmail.com>
2021-08-27 14:06:53 -0700

    redis set and get functions passing tests

--- __tests__/redis.test.js
+++ __tests__/redis.test.js
@@ -1,5 +1,6 @@
 const redis = require('redis')
 const { promisify } = require('util')
+const { setGameData, getGameData } = require('../lib/utils/redis.js')
 let redisClient;
 let get;
 let set;
@@ -7,16 +8,32 @@ let set;
 
 describe('tests redis functions', () => {
   beforeEach(() => {
-    redisClient = redis.createClient()
+    redisClient = redis.createClient(6379, '127.0.0.1')
+    redisClient.flushall('ASYNC', function (err) {
+      console.log(err)
+    })
     set = promisify(redisClient.set).bind(redisClient)
     get = promisify(redisClient.get).bind(redisClient)
+
   })
 
+  // afterEach(() => {
+  //   redisClient.end(true)
+  // })
+
   it('sets a game into cache', async () => {
-    const newSet = await set('chase', 'abbott')
+    await setGameData(redisClient, 'chase', { chase: 'abbott' })
 
-    console.log(newSet)
+    const getData = await get('chase')
 
-    expect(await get('chase')).toBe('abbott')
+    expect(JSON.parse(getData)).toEqual({ chase: 'abbott' })
+  })
+
+  it('gets specific set of data from the cache', async () => {
+    await setGameData(redisClient, 'chase', { chase: 'abbott' })
+
+    const retrievedData = await getGameData(redisClient, 'chase')
+
+    expect(retrievedData).toEqual({ chase: 'abbott' })
   })
 })
