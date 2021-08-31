edf0404eb18930489b0e99dffb4735ca92a8d95c
Chase <cabbott93@gmail.com>
2021-08-31 11:51:49 -0700

    add deployed front end to cors origin

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const GameService = require("./lib/services/GameService.js");
 const httpServer = require("http").createServer(app);
 const pool = require("./lib/utils/pool.js");
 const io = require("socket.io")(httpServer, {
-  cors: true,
+  cors: {
+    origin: ['https://zilch-v2-staging.netlify.app']
+  }
 });
 const {
   setGameData,
