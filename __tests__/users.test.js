2aa73de63f92b80b12f4d292d31e9995c070dd07
Katherine Tam <katherinemtam@gmail.com>
2021-09-01 00:08:18 -0700

    users getById route test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -442,15 +442,28 @@ describe('users routes', () => {
     }])
   })
 
-  test.only('get user info by username', async() => {
+  test('get user info by username', async() => {
     const user1 = await agent
       .post('/api/v1/signup')
       .send(userOne);
 
-      console.log('user1', user1.body)
-      console.log('user1 username', user1.body.username)
     const res = await agent
       .get(`/api/v1/users/${user1.body.username}`)
+
+    expect(res.body).toEqual({
+      userId: '1',
+      username: 'username',
+      avatar: 'Avatar.png'
+    })
+  })
+
+  test('get user info by id', async() => {
+    const user1 = await agent
+      .post('/api/v1/signup')
+      .send(userOne);
+
+    const res = await agent
+      .get(`/api/v1/users/id/${user1.body.userId}`)
 
     expect(res.body).toEqual({
       userId: '1',
